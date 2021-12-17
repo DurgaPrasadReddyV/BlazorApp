653e5590a82363658ee3215e7baa5cb79fff9f88
Durga Prasad Reddy Vennapusa <durgaprasad.vennapusa@gmail.com>
2021-12-17 19:07:28 +0530

    add migrations

--- src/BlazorApp.WebUI/Server/Program.cs
+++ src/BlazorApp.WebUI/Server/Program.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlazorAppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    //Note: Microsoft recommends to NOT migrate your database at Startup. 
+    //You should consider your migration strategy according to the guidelines
+    serviceScope.ServiceProvider.GetService<BlazorAppDbContext>().Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,9 +39,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
+app.UseSwagger();
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
-
+app.UseSwaggerUI();
 app.Run();
